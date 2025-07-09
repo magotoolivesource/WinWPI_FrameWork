7405e4bafef9a57ba01d94ab9987b5c362b500f4
학교컴3번 <magotoolivesource@gmail.com>
2025-07-09 09:11:27 +0900

    transform 매트릭스 값 지정하는방식으로 기본 수정

--- FrameWork_2DAPI/Compoment/Transform.cpp
+++ FrameWork_2DAPI/Compoment/Transform.cpp
@@ -1,2 +1,41 @@
-#include "Transform.h"
+﻿#include "Transform.h"
 
+void Transform::UpdateIfDirty( ) {
+	if ( !isDirty ) return;
+
+	if ( parent ) {
+		cachedWorldPosition = parent->GetWorldPosition( );
+		cachedWorldPosition.x += localposition.x;
+		cachedWorldPosition.y += localposition.y;
+
+		cachedWorldRotation = parent->GetWorldRotation( ) + rotation;
+
+		auto parentScale = parent->GetWorldScale( );
+		cachedWorldScale.x = parentScale.x * localScale.x;
+		cachedWorldScale.y = parentScale.y * localScale.y;
+
+		cachedWorldDepth = parent->GetWorldDepth( ) + zdepth;
+	}
+	else {
+		cachedWorldPosition = localposition;
+		//cachedWorldRotation = localRotation;
+		//cachedWorldScale = localScale;
+		cachedWorldDepth = zdepth;
+	}
+
+	// Build transformation matrix
+	Gdiplus::Matrix matrix;
+	matrix.Translate(cachedWorldPosition.x, cachedWorldPosition.y);
+	matrix.Rotate(cachedWorldRotation);
+	matrix.Scale(cachedWorldScale.x, cachedWorldScale.y);
+	//cachedWorldMatrix = matrix;
+
+	matrix.GetElements(m_tempcachedmatrix);
+	cachedWorldMatrix.SetElements(
+		m_tempcachedmatrix[ 0 ], m_tempcachedmatrix[ 1 ]
+		, m_tempcachedmatrix[ 2 ], m_tempcachedmatrix[ 3 ]
+		, m_tempcachedmatrix[ 4 ], m_tempcachedmatrix[ 5 ]
+	);
+
+	isDirty = false;
+}
--- FrameWork_2DAPI/Compoment/Transform.h
+++ FrameWork_2DAPI/Compoment/Transform.h
@@ -2,6 +2,13 @@
 #include "Component.h"
 #include "../Core/Vector.h"
 
+#include <cmath>
+#include <algorithm>
+#include <windows.h>
+#include <gdiplus.h>
+
+
+
 class Transform : public Component
 {
     // new Transform 막기위한것
@@ -19,21 +26,29 @@ protected:
 
 public:
     Vec2 localposition;
-	Vec2 world_position;
+	//Vec2 world_position;
 	float zdepth = 0; // Z-order for rendering
     float width = 50.0f;
     float height = 50.0f;
     float rotation = 0.0f; // in degrees
 
 
+	Vec2 localScale = { 1, 1 };
+
 private:
 	Transform* parent = nullptr;
 	std::vector<Transform*> children;
 
-	Vec2 cachedWorldPosition = { 0, 0 };
-	float cachedWorldDepth = 0.0f;
-	bool isDirty = true;
 
+	bool isDirty = true;
+	Vec2 cachedWorldPosition = { 0, 0 };
+	float cachedWorldRotation = 0.0f;
+	Vec2 cachedWorldScale = { 1, 1 };
+	float cachedWorldDepth = 0.0f;
+	
+	Gdiplus::Matrix cachedWorldMatrix;
+
+	float m_tempcachedmatrix[ 6 ] = { 0, 0, 0, 0, 0, 0 };
 protected:
 	void MarkDirty( ) {
 		isDirty = true;
@@ -42,6 +57,8 @@ protected:
 		}
 	}
 
+	void UpdateIfDirty( );
+
 public:
 	void SetDepth(float d) {
 		zdepth = d;
@@ -49,6 +66,7 @@ public:
 	}
 
 	float GetDepth( ) const {
+		//UpdateIfDirty( );
 		return zdepth;
 	}
 
@@ -66,37 +84,64 @@ public:
 		MarkDirty( );
 	}
 
+	float GetWorldRotation( ) {
+		UpdateIfDirty( );
+		return cachedWorldRotation;
+	}
+
+	Vec2& GetWorldScale( ) {
+		UpdateIfDirty( );
+		return cachedWorldScale;
+	}
+
+	float GetWorldDepth( ) {
+		UpdateIfDirty( );
+		return cachedWorldDepth;
+	}
+
+	Gdiplus::Matrix& GetWorldMatrix( ) {
+		UpdateIfDirty( );
+		return cachedWorldMatrix;
+	}
+
+
+
 	Vec2 GetWorldPosition( ) {
 		//if ( !parent ) return localposition;
 		//auto p = parent->GetWorldPosition( );
 		//return { p.x + localposition.x, p.y + localposition.y };
 
-		if ( isDirty ) {
-			if ( parent ) {
-				auto p = parent->GetWorldPosition( );
-				cachedWorldPosition = { p.x + localposition.x, p.y + localposition.y };
-			}
-			else {
-				cachedWorldPosition = localposition;
-			}
-			isDirty = false;
-		}
+		//if ( isDirty ) {
+		//	if ( parent ) {
+		//		auto p = parent->GetWorldPosition( );
+		//		cachedWorldPosition = { p.x + localposition.x, p.y + localposition.y };
+		//	}
+		//	else {
+		//		cachedWorldPosition = localposition;
+		//	}
+		//	isDirty = false;
+		//}
+
+		UpdateIfDirty( );
 		return cachedWorldPosition;
 	}
-	float GetWorldDepth( ) {
-		//if ( !parent ) return zdepth;
-		//return parent->GetWorldDepth( ) + zdepth;
 
-		if ( isDirty ) {
-			if ( parent ) {
-				cachedWorldDepth = parent->GetWorldDepth( ) + zdepth;
-			}
-			else {
-				cachedWorldDepth = zdepth;
-			}
-		}
-		return cachedWorldDepth;
-	}
+
+	//float GetWorldDepth( ) {
+	//	//if ( !parent ) return zdepth;
+	//	//return parent->GetWorldDepth( ) + zdepth;
+
+	//	if ( isDirty ) {
+	//		if ( parent ) {
+	//			cachedWorldDepth = parent->GetWorldDepth( ) + zdepth;
+	//		}
+	//		else {
+	//			cachedWorldDepth = zdepth;
+	//		}
+	//	}
+	//	return cachedWorldDepth;
+	//}
+
 	void UpdateAllPositions( )
 	{
 		// 현재 위치 값 바뀌면 내 위치에서부터 하단값들 무조건 바꾸도록 처리하기
@@ -106,12 +151,12 @@ public:
     // Getters
     Vec2 getLocalPosition() const { return localposition; }
     // Setters
-    void setLocalPosition(const Vec2& pos) 
+    void setLocalPosition(const Vec2& pos)
 	{ 
 		localposition = pos; 
 		MarkDirty( );
 	}
-    void setLocalPosition(int p_x, int p_y) 
+    void setLocalPosition(int p_x, int p_y)
     { 
         localposition.x = p_x;
         localposition.y = p_y;
@@ -123,9 +168,31 @@ public:
     void setWidth(float w) { width = w; }
     void setHeight(float h) { height = h; }
 
+
+	//void SetDepth(float d) {
+	//	zdepth = d;
+	//	MarkDirty( );
+	//}
+
 	float getRotation( ) const { return rotation; }
-    void setRotation(float rot) { rotation = rot; }
+    void setRotation(float rot) 
+	{ 
+		rotation = rot; 
+		MarkDirty( );
+	}
     // Override the clone method
 	//virtual Transform* clone() const override { return new Transform(*this); }
+
+
+	void SetLocalRotation(float angleDegrees) {
+		rotation = angleDegrees;
+		MarkDirty( );
+	}
+
+	void SetLocalScale(Vec2 scale) {
+		localScale = scale;
+		MarkDirty( );
+	}
+
 };
 
